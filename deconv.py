a72e6ac15f0cf1c796e3eb96574d1542651d4ac6
pradyuprasad <pradyu.sp@gmail.com>
2025-01-11 13:01:47 +0800

    save to json

--- deconv.py
+++ deconv.py
@@ -1,4 +1,4 @@
-from typing import Dict, Generic, Tuple, TypeVar
+from typing import Dict, Generic, List, Tuple, TypeVar
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,6 +6,7 @@ from load_data import load_test_data_for_plotting
 from CNN import CNN
 from torch.utils.data import DataLoader
 import einops
+import json
 B = TypeVar('B')
 NUM_FILTERS = 32
 NUM_CHANNELS = 3
@@ -34,8 +35,8 @@ def reconstruct_image_from_first_layer(x: FirstLayer[B], deconv_layer: nn.ConvTr
     output = deconv_layer(x.tensor)
     return ReconstructedImage(output)
 
-def get_dataloader_of_specific_class() -> DataLoader:
-    return load_test_data_for_plotting(batch_size=32, allowed_classes=[0])
+def get_dataloader_of_specific_class(allowed_classes: List[int]) -> DataLoader:
+    return load_test_data_for_plotting(batch_size=32, allowed_classes=allowed_classes)
 
 def get_max_activation_coords(tensor: torch.Tensor) -> Tuple[int, int]:
     """Takes a 2D tensor and returns the (height, width) coordinates of its maximum value"""
@@ -52,7 +53,7 @@ def reconstruct_wrapper() -> None:
     deconv: nn.ConvTranspose2d = nn.ConvTranspose2d(in_channels=NUM_FILTERS, out_channels=NUM_CHANNELS, kernel_size=3, padding=1, bias=False)
     deconv = deconv.to(device)
     deconv.weight = nn.Parameter(model.conv1.conv.weight)
-    test_dataloader: DataLoader = get_dataloader_of_specific_class()
+    test_dataloader: DataLoader = get_dataloader_of_specific_class(allowed_classes=[0])
     images, labels = next(iter(test_dataloader))
     images = images.to(device)
 
@@ -71,8 +72,6 @@ def reconstruct_wrapper() -> None:
 
     feature_map_current_image: torch.Tensor = activations['conv1'][image_index]
     for i in range(len(images)):
-        if i > 0:
-            break
         feature_map_current_filter = feature_map_current_image[i]
         assert feature_map_current_filter.shape == torch.Size([96, 96]), f"Expected [96, 96], got {feature_map_current_image.shape}"
         max_activ_x, max_activ_y = get_max_activation_coords(feature_map_current_filter)
@@ -83,15 +82,14 @@ def reconstruct_wrapper() -> None:
             activations_to_deconv = activations_to_deconv.to(device)
             projection_of_this_filter = deconv(activations_to_deconv)
             projection_of_this_filter = F.relu(projection_of_this_filter)
-            print(projection_of_this_filter)
+            projection_to_save = einops.rearrange( projection_of_this_filter ,"b c h w -> h w (b c)")
+            tensor_to_json: List = projection_to_save.tolist()
+            assert len(tensor_to_json) == 96
+            assert len(tensor_to_json[0]) == 96
+            assert len(tensor_to_json[0][0]) == 3
+            with open(f"image_{image_index}_filter{i}.json", "w") as f:
+                json.dump(tensor_to_json, f)
+            print(f"done with filter {i+1}")
 
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    reconstruct_wrapper()
